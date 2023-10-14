11c50f60199920507d4b694bfa6b11846fbaf46e
Joseph <113116981+Jexcode@users.noreply.github.com>
2023-10-14 20:05:47 +0100

    Update app.js

--- Js/app.js
+++ Js/app.js
@@ -81,3 +81,68 @@ const intvl = setInterval(() => {
   }
 
 }, 1000);
+
+const portfolioTabItemOne = document.querySelector('.portfolio-tab-item-one'); 
+const portfolioTabItemTwo = document.querySelector('.portfolio-tab-item-two'); 
+const clipBg = document.querySelector(".click-bg");
+const portfolioItems = document.querySelectorAll('.portfolio-var');
+
+const barCons = document.querySelectorAll('.skill-bar');
+const bars = document.querySelectorAll('.bar');
+
+
+
+portfolioTabItemOne.addEventListener('click', ()=>{
+  const id = portfolioItems[0].dataset.id;
+  
+    clipBg.classList.add('active-one');
+    clipBg.classList.remove('active-two');
+    portfolioItems[0].classList.add('show-active');
+    portfolioItems[1].classList.remove('show-active');
+    });
+    
+    
+
+
+portfolioTabItemTwo.addEventListener('click', (e)=>{
+  const id = e.target.dataset.id;
+  
+  
+    clipBg.classList.add('active-two');
+    clipBg.classList.remove('active-one');
+    portfolioItems[0].classList.remove('show-active');
+    portfolioItems[1].classList.add('show-active');
+    
+         bars.forEach(function(bar) {
+           bar.classList.add('grow')
+         });
+     
+});
+
+
+const themeControl = document.querySelector('.theme-control');
+
+themeControl.addEventListener('click', ()=>{
+  document.body.classList.toggle('dark-theme');
+  
+  const body = document.body;
+  
+  if (body.classList.contains("dark-theme")) {
+  themeControl.innerHTML = '<i class="fa-solid fa-sun"></i>';
+  } else {
+  themeControl.innerHTML = '<i class="fa-solid fa-moon"></i>';
+  }
+});
+
+
+function show(){
+  barCons.forEach(function(barCon) {
+     barCon.addEventListener("show", () => {
+      bars.forEach(function(bar) {
+         bar.classList.toggle('grow')
+      });
+     });
+  }); 
+
+}
+
